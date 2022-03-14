45ca9880cd106345773063d584117336bc057a3c
Christian Herber <christian@tangaradaserra.mt.gov.br>
2022-03-14 13:21:25 -0400

    add admob

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { AdMobBanner, setTestDeviceIDAsync } from "expo-ads-admob";
 
 import Cep from './src/components/Cep'
 import Header from './src/components/Header';
@@ -21,6 +22,13 @@ export default function App() {
       <Header />
       <SafeAreaView style={styles.container}>
         <Cep />
+        <AdMobBanner
+          bannerSize='fullBanner'
+          adUnitID='ca-app-pub-3032945660168947~6273175400'
+          setTestDeviceIDAsync
+          servePersonalizedAds
+          onDidFailToReceiveAdWithError={ (err) => console.log(err) }
+        />
       </SafeAreaView>
     </PaperProvider>
   );
